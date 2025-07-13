fb6a4c1cb48bd3eb304aba8f76f13cee86f770da
kleinyumoretti <1003632472@qq.com>
2025-07-13 16:24:43 +0800

    实现分页查询功能 实现员工启用禁用功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -12,10 +14,7 @@ import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -83,6 +82,23 @@ public class EmployeeController {
     public Result save(@RequestBody EmployeeDTO employeeDTO){
         log.info("新增员工，员工数据：{}",employeeDTO);
         employeeService.save(employeeDTO);
+        return Result.success();
+    }
+
+
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询，参数：{}",employeePageQueryDTO);
+        PageResult pagesult=employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pagesult);
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("员工启用禁用")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("员工状态：{},{}",status,id);
+        employeeService.startOrStop(status,id);
         return Result.success();
     }
 }
